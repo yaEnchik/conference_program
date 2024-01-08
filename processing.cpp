0cf553f04387c2639964f4b4a8cc6e9406fc8afd
yaEnchik <anaaniskova7@gmail.com>
2024-01-08 15:00:58 +0300

    реализован поиск самой длиной конференции

--- processing.cpp
+++ processing.cpp
@@ -2,7 +2,12 @@
 
 #include "processing.h"
 
-conference_structure* get_longest_theme(conference_structure* array[], int (*check)(conference_structure* first, conference_structure* second)) {
-	conference_structure* result = new conference_structure();
+conference_structure* get_longest_theme(conference_structure* array[], int size, int (*check)(conference_structure* first, conference_structure* second)) {
+	conference_structure* result = array[0];
+	for (int i = 0; i <= size; i++) {
+		if (check(array[i], result) == 1) {
+			result = array[i];
+		}
+	}
 	return result;
 }
