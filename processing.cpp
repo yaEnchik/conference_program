18e5c7c2a773283b792f03671cb3753bcfd6de05
yaEnchik <anaaniskova7@gmail.com>
2024-01-08 15:44:57 +0300

    изменен метод поиска максимального

--- processing.cpp
+++ processing.cpp
@@ -2,10 +2,10 @@
 
 #include "processing.h"
 
-conference_structure* get_longest_theme(conference_structure* array[], int size, int (*check)(conference_structure* first, conference_structure* second)) {
+conference_structure* get_longest_theme(conference_structure* array[], int size) {
 	conference_structure* result = array[0];
-	for (int i = 0; i <= size; i++) {
-		if (check(array[i], result) == 1) {
+	for (int i = 0; i <= size - 1; i++) {
+		if (compare_by_time(array[i], result) == 1) {
 			result = array[i];
 		}
 	}
